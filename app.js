6808369d2194c586fc2b62b7f52d9dd6aa9d21fa
adremarie <adreanne.marie@gmail.com>
2016-01-28 20:42:04 +0000

    node.js lab

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var project = require('./routes/project');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -35,10 +37,9 @@ if ('development' == app.get('env')) {
 }
 
 // Add routes here
-app.get('/', index.view);
-// Example route
-// app.get('/users', user.list);
 
+app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
---
+++ routes/project.js
@@ -0,0 +1,7 @@
+exports.viewProject = function(req, res) { 
+var name = req.params.name;
+    res.render('project', {
+    	'projectName': name
+    	
+    });
+};
